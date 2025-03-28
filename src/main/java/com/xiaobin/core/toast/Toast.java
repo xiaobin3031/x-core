b2a6cd4ad1814538b148f5f7b7e14b24128922a2
weibin.xu <xuwb@shanshan-business.com>
2025-03-28 14:25:19 +0800

    消息框支持3个级别的消息，且支持过滤

--- src/main/java/com/xiaobin/core/toast/Toast.java
+++ src/main/java/com/xiaobin/core/toast/Toast.java
@@ -1,6 +1,7 @@
 package com.xiaobin.core.toast;
 
 import com.xiaobin.core.json.JSON;
+import com.xiaobin.core.log.SysLogUtil;
 import com.xiaobin.core.server.MyHttpHandler;
 import com.xiaobin.core.server.MyHttpServer;
 import com.xiaobin.core.toast.model.ToastModel;
@@ -10,8 +11,9 @@ import java.awt.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -20,26 +22,44 @@ import java.util.concurrent.atomic.AtomicLong;
  */
 public class Toast {
 
-    private static JFrame TOAST_FRAME;
-    private static JPanel MSG_PANEL;
-    private final static JLabel hourField = new JLabel();
-    private final static JLabel minuteField = new JLabel();
-    private final static JLabel secondField = new JLabel();
-    private final static JLabel ymdField = new JLabel();
+    private static final int level_debug = 1;
+    private static final int level_warn = 100;
+    private static final int level_error = 200;
 
-    private static int hour;
-    private static int minute;
-    private static int seconds;
+    private static final Map<Integer, String> levelMap = Map.of(
+            level_debug, "debug",
+            level_warn, "warn",
+            level_error, "error"
+    );
 
-    private static int msgCount = 0;
+    private final static Toast instance = new Toast();
+
+    private JFrame TOAST_FRAME;
+    private JPanel MSG_PANEL;
+    private final JLabel hourField = new JLabel();
+    private final JLabel minuteField = new JLabel();
+    private final JLabel secondField = new JLabel();
+    private final JLabel ymdField = new JLabel();
+    private final Set<Integer> curLevel = new HashSet<>();
+
+    private final List<Integer> msgLevelList = new ArrayList<>();
+    private final List<JTextArea> msgList = new ArrayList<>();
+
+    private int hour;
+    private int minute;
+    private int seconds;
+
+    private int msgCount = 0;
 
     static {
-        init();
-
         MyHttpServer.addContext("/notice", new ToastHttpHandle());
     }
 
-    private static void init() {
+    private Toast(){
+        init();
+    }
+
+    private void init() {
         JFrame jFrame = new JFrame("toast");
         jFrame.setLocationRelativeTo(null);
         jFrame.setSize(350, 200);
@@ -47,6 +67,40 @@ public class Toast {
 
         Container container = jFrame.getContentPane();
         container.setLayout(new BorderLayout());
+
+        JPanel levelPanel = new JPanel();
+        levelPanel.setLayout(new FlowLayout());
+        JCheckBox debugCheckBox = new JCheckBox("debug");
+        debugCheckBox.addActionListener(e -> {
+            if (debugCheckBox.isSelected()) {
+                curLevel.add(level_debug);
+            } else {
+                curLevel.remove(level_debug);
+            }
+            refreshMsgPanel();
+        });
+        JCheckBox warnCheckBox = new JCheckBox("warn");
+        warnCheckBox.addActionListener(e -> {
+            if (warnCheckBox.isSelected()) {
+                curLevel.add(level_warn);
+            } else {
+                curLevel.remove(level_warn);
+            }
+            refreshMsgPanel();
+        });
+        JCheckBox errorCheckBox = new JCheckBox("error");
+        errorCheckBox.addActionListener(e -> {
+            if (errorCheckBox.isSelected()) {
+                curLevel.add(level_error);
+            } else {
+                curLevel.remove(level_error);
+            }
+            refreshMsgPanel();
+        });
+        levelPanel.add(debugCheckBox);
+        levelPanel.add(warnCheckBox);
+        levelPanel.add(errorCheckBox);
+        container.add(levelPanel, BorderLayout.NORTH);
 
         JPanel center = new JPanel();
         center.setLayout(new BorderLayout());
@@ -67,7 +121,31 @@ public class Toast {
         TOAST_FRAME = jFrame;
     }
 
-    private static JPanel initBtnPanel() {
+    private void refreshMsgPanel() {
+        clearMsgPanel();
+        List<Integer> indexList = new ArrayList<>();
+        for (int i = 0; i < msgLevelList.size(); i++) {
+            if (matchLevel(msgLevelList.get(i))) {
+                indexList.add(i);
+            }
+        }
+        msgCount = indexList.size();
+        if (msgCount == 0) {
+            msgCount++;
+        }
+        GridLayout layout = (GridLayout) MSG_PANEL.getLayout();
+        layout.setRows(msgCount);
+        for (Integer i : indexList) {
+            MSG_PANEL.add(msgList.get(i));
+        }
+        MSG_PANEL.repaint();
+    }
+
+    private boolean matchLevel(int level) {
+        return curLevel.isEmpty() || curLevel.contains(level);
+    }
+
+    private JPanel initBtnPanel() {
         JPanel btnPanel = new JPanel();
         JButton jClearBtn = new JButton("Clear");
         jClearBtn.addActionListener(e -> {
@@ -91,12 +169,14 @@ public class Toast {
         return btnPanel;
     }
 
-    private static void clearMsgPanel() {
-        MSG_PANEL.removeAll();
+    private void clearMsgPanel() {
+        for (Component component : MSG_PANEL.getComponents()) {
+            MSG_PANEL.remove(component);
+        }
         msgCount = 0;
     }
 
-    private static JPanel initToolbar() {
+    private JPanel initToolbar() {
         JPanel toolBar = new JPanel();
         toolBar.setLayout(new FlowLayout(FlowLayout.LEFT));
         refreshYmd();
@@ -140,11 +220,11 @@ public class Toast {
         return toolBar;
     }
 
-    private static void refreshYmd() {
+    private void refreshYmd() {
         ymdField.setText(LocalDate.now().format(DateTimeFormatter.ISO_DATE) + " ");
     }
 
-    private static void setTimeField(JLabel jLabel, int time) {
+    private void setTimeField(JLabel jLabel, int time) {
         if (time < 10) {
             jLabel.setText("0" + time);
         } else {
@@ -153,23 +233,51 @@ public class Toast {
     }
 
     public static void show(String message) {
-        GridLayout layout = (GridLayout) MSG_PANEL.getLayout();
-        if (layout.getRows() <= msgCount) {
-            layout.setRows(layout.getRows() + 10);
-        }
-        JTextArea jText = new JTextArea(formatMsg(message));
+        instance.addToPane(level_debug, message);
+    }
+
+    public static void showDebug(String message) {
+        instance.addToPane(level_debug, message);
+    }
+
+    public static void showWarn(String message) {
+        instance.addToPane(level_warn, message);
+    }
+
+    public static void showError(String message) {
+        instance.addToPane(level_error, message);
+    }
+
+    private void addToPane(int level, String message) {
+        String s = formatMsg(level, message);
+        JTextArea jText = new JTextArea(s);
         jText.setEditable(false);
-        MSG_PANEL.add(jText);
-        msgCount++;
+        msgLevelList.add(level);
+        msgList.add(jText);
+        if (matchLevel(level)) {
+            GridLayout layout = (GridLayout) MSG_PANEL.getLayout();
+            if (layout.getRows() <= msgCount) {
+                layout.setRows(layout.getRows() + 10);
+            }
+            MSG_PANEL.add(jText);
+            msgCount++;
+        }
         if (!TOAST_FRAME.isVisible()) {
             TOAST_FRAME.setVisible(true);
         }
-        System.out.println("show message: " + message);
+        SysLogUtil.logNormalF("show message [%s]: %s", levelMap.get(level), message);
     }
 
-    private static String formatMsg(String msg) {
+    private String formatMsg(String msg) {
         if (msg != null) {
             msg = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")) + "   " + msg;
+        }
+        return msg;
+    }
+
+    private String formatMsg(int level, String msg) {
+        if (msg != null) {
+            msg = String.format("%s [%s]   %s", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")), levelMap.get(level), msg);
         }
         return msg;
     }
